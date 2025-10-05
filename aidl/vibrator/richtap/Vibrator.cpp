2880b584691d3a5254ca8c9644f847acb6fc427c
Aman Singh <amankumarsingh1412@gmail.com>
2025-10-05 17:44:31 +0530

    richtap: Play POP effect for CLICK and TICK
    Signed-off-by: Aman Singh <amankumarsingh1412@gmail.com>

--- aidl/vibrator/richtap/Vibrator.cpp
+++ aidl/vibrator/richtap/Vibrator.cpp
@@ -77,7 +77,7 @@ ndk::ScopedAStatus Vibrator::perform(Effect effect, EffectStrength es,
     int32_t strength;
 
     if (effect == Effect::TICK || effect == Effect::CLICK)
-        effect = Effect::THUD;
+        effect = Effect::POP;
 
     if (effect < Effect::CLICK || effect > Effect::HEAVY_CLICK)
         return ndk::ScopedAStatus(AStatus_fromExceptionCode(EX_UNSUPPORTED_OPERATION));
