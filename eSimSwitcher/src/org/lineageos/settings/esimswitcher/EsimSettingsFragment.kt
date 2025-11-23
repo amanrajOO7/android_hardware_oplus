8a5d431d2412128f6a721dc4eabd8b8096f410e4
Bruno Martins <bgcngm@gmail.com>
2025-11-23 21:20:08 +0000

    eSimSwitcher: Convert for M3E support
    Change-Id: Icc8b237ea4ba144f1b26e3cd2de965031db22589

--- eSimSwitcher/src/org/lineageos/settings/esimswitcher/EsimSettingsFragment.kt
+++ eSimSwitcher/src/org/lineageos/settings/esimswitcher/EsimSettingsFragment.kt
@@ -8,10 +8,10 @@ package org.lineageos.settings.esimswitcher
 import android.app.AlertDialog
 import android.os.Bundle
 import androidx.preference.Preference
-import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.SwitchPreferenceCompat
+import com.android.settingslib.widget.SettingsBasePreferenceFragment
 
-class EsimSettingsFragment : PreferenceFragmentCompat(), Preference.OnPreferenceChangeListener {
+class EsimSettingsFragment : SettingsBasePreferenceFragment(), Preference.OnPreferenceChangeListener {
     private lateinit var controller: EsimController
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
